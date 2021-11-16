eb1f5a21ef3d0c33b32717ed089b6645e9df507d
Jay Gudsson <gudsson@gmail.com>
2021-11-15 20:18:34 -0700

    courtesy commit

--- client/src/lib/apiClient.js
+++ client/src/lib/apiClient.js
@@ -9,13 +9,14 @@ const apiClient = {
   async getTimeData() {
     console.log("getting time data...");
     let response = await axios.get(
-      "http://localhost:4005/api/monitor/queries",
+      "http://localhost:4005/api/monitor/queries?minutes=69",
       {
         headers: {
           authorization:
             "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjE5Mjc5MzUzMWU2NzdhYzQwNWU2OTgzIiwiaWF0IjoxNjM2OTg5MjM3LCJleHAiOjE2MzcwMjUyMzd9.6g3ecEGG1-_8u7OyNEYG6pogWmay7XlhXd4Kjebrbis",
           queryMinutes: 43800,
         },
+        query: 43800,
       }
     );
     return response;
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,74 +2,89 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("express-jwt");
-const jsonwebtoken = require('jsonwebtoken');
-const cors = require('cors');
-require("dotenv").config()
+const jsonwebtoken = require("jsonwebtoken");
+const cors = require("cors");
+require("dotenv").config();
 
 const configController = require("../controllers/configController");
 const monitoringController = require("../controllers/monitoringController");
-const { User } = require("../models/User")
-
+const { User } = require("../models/User");
 
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
 
   jsonwebtoken.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(err)
-    if (err) return res.sendStatus(403)
-    console.log(user)
-    req.user = user
-    next()
-  })
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    console.log(user);
+    req.user = user;
+    next();
+  });
 }
 
 const testJWT = (req, res) => {
-  res.status(200).json({message: "IT WORKED :)"})
-}
+  res.status(200).json({ message: "IT WORKED :)" });
+};
 
-router.post("/config", authenticateToken, configController.createConfig)
+router.post("/config", authenticateToken, configController.createConfig);
 
-router.get("/monitor/resolvers", monitoringController.getResolverData)
+router.get("/monitor/resolvers", monitoringController.getResolverData);
 
-router.get("/monitor/queries", authenticateToken, monitoringController.getIndividualQueryData)
+// router.get("/monitor/queries", monitoringController.getIndividualQueryData);
+router.get(
+  "/monitor/queries",
+  authenticateToken,
+  monitoringController.getIndividualQueryData
+);
 
-router.get("/testjwt", authenticateToken, testJWT)
+router.get("/testjwt", authenticateToken, testJWT);
 
 // Create new user
-router.post('/users', async (req, res) => {
+router.post("/users", async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const user = { username: req.body.name, password: hashedPassword };
-    console.log(user)
+    console.log(user);
     let userObj = await User.create(user); // push to "real" database
-    res.status(201).json({token: jsonwebtoken.sign({user: userObj._id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '10h'})})
-  } catch(err) {
-    console.log(err)
-    res.status(500).json({error: "Something went wrong"});
+    res.status(201).json({
+      token: jsonwebtoken.sign(
+        { user: userObj._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "10h" }
+      ),
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Something went wrong" });
   }
 });
 
-
 // Authenticate login
-router.post('/users/login', async (req, res) => {
-  const user = User.find({username: req.body.name}); // find user from "real" database
+router.post("/users/login", async (req, res) => {
+  const user = User.find({ username: req.body.name }); // find user from "real" database
   if (!user) {
-    return res.status(400).send('User does not exist');
+    return res.status(400).send("User does not exist");
   }
 
   try {
     if (await bcrypt.compare(req.body.password, user.password)) {
       // Give back JWT
-      console.log(user._id)
+      console.log(user._id);
 
-      res.status(201).json({token: jsonwebtoken.sign({user: user._id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '10h'})})
+      res.status(201).json({
+        token: jsonwebtoken.sign(
+          { user: user._id },
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: "10h" }
+        ),
+      });
     } else {
-      res.send('Login > Failed');
+      res.send("Login > Failed");
     }
   } catch {
-    res.status(500).send()
+    res.status(500).send();
   }
 });
 
